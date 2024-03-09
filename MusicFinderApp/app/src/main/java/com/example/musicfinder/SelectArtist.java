cd78d6f5c063371b14465c3012e5f36d57ba2bc4
ycho0097 <52307200+Octagoat@users.noreply.github.com>
2024-03-09 22:33:11 +1100

    removed code duplication

--- MusicFinderApp/app/src/main/java/com/example/musicfinder/SelectArtist.java
+++ MusicFinderApp/app/src/main/java/com/example/musicfinder/SelectArtist.java
@@ -6,7 +6,6 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -19,43 +18,15 @@ import java.util.List;
 public class SelectArtist extends AppCompatActivity {
     private int count;
 
-    // Define constant page names
-    public static final String PAGE_ARTIST = "Artist";
-    public static final String PAGE_GENRE = "Genre";
-    public static final String PAGE_TIME_PERIOD = "Time Period";
-    public static final String PAGE_MOOD = "Occasion or\nMood";
-
-    public static final String PAGE_ADDITIONAL = "Additional Filters";
-
-    // Define constant page descriptions
-    public static final String DESC_ARTIST = "Start your music journey by\nselecting a specific artist or band. ";
-    public static final String DESC_GENRE = "it's time to define the genre you're\ninterested in.";
-    public static final String DESC_TIME_PERIOD = "Specify the time period you prefer.";
-    public static final String DESC_MOOD = "Customise your playlist for specific\nsituations or emotional states. ";
-    public static final String DESC_ADDITIONAL = "Additional filters to refine the music\nrecommendations.";
-
-    //  Define constant text view hints
-    public static final String HINT_ARTIST = "Enter an artist/band...";
-    public static final String HINT_GENRE = "Enter a genre...";
-    public static final String HINT_TIME_PERIOD = "Choose a specific decade...";
-    public static final String HINT_MOOD = "Describe the occasion/mood...";
-    public static final String HINT_ADDITIONAL = "Filters to apply...";
-
-
     // Create constant list of page title names
-    public static final List<String> PAGE_NAMES = Collections.unmodifiableList(
-            Arrays.asList(PAGE_ARTIST, PAGE_GENRE, PAGE_TIME_PERIOD, PAGE_MOOD, PAGE_ADDITIONAL)
-    );
+    private List<String> PAGE_NAMES;
+
 
     // Create constant list of page descriptions
-    public static final List<String> PAGE_DESCRIPTIONS = Collections.unmodifiableList(
-            Arrays.asList(DESC_ARTIST, DESC_GENRE, DESC_TIME_PERIOD, DESC_MOOD, DESC_ADDITIONAL)
-    );
+    private List<String> PAGE_DESCRIPTIONS;
 
     // Create constant list of edit text hints
-    public static final List<String> PAGE_TEXT_HINTS = Collections.unmodifiableList(
-            Arrays.asList(HINT_ARTIST, HINT_GENRE, HINT_TIME_PERIOD, HINT_MOOD, HINT_ADDITIONAL)
-    );
+    private List<String> PAGE_TEXT_HINTS;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -66,6 +37,16 @@ public class SelectArtist extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        PAGE_NAMES = Collections.unmodifiableList(
+                Arrays.asList(getResources().getStringArray(R.array.title_name))
+        );
+        PAGE_DESCRIPTIONS = Collections.unmodifiableList(
+                Arrays.asList(getResources().getStringArray(R.array.page_desc))
+        );
+        PAGE_TEXT_HINTS = Collections.unmodifiableList(
+                Arrays.asList(getResources().getStringArray(R.array.page_hint))
+        );
 
         count = 0;
 
@@ -92,8 +73,6 @@ public class SelectArtist extends AppCompatActivity {
         if (count == PAGE_NAMES.size()) {
             // TODO: open the page where prompts user to input amount of songs
 
-
-            return;
         }
         else {
             // TODO: store the info on the database or locally to parse to LLM
