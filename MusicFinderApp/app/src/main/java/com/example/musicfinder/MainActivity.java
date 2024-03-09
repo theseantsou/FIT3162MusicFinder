2726cda2e2244a5d1ac1638c3b1b2a7d54002962
ycho0097 <52307200+Octagoat@users.noreply.github.com>
2024-03-09 19:31:06 +1100

    Main page UI design

--- MusicFinderApp/app/src/main/java/com/example/musicfinder/MainActivity.java
+++ MusicFinderApp/app/src/main/java/com/example/musicfinder/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.musicfinder;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +21,9 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    public void openArtistPage(View view) {
+        
     }
 }
