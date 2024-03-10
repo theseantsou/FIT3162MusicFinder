cfc9588551c94630b0958513e3191e986f746422
ycho0097 <52307200+Octagoat@users.noreply.github.com>
2024-03-10 17:54:30 +1100

    added navigation bar to home screen

--- MusicFinderApp/app/src/main/java/com/example/musicfinder/MainActivity.java
+++ MusicFinderApp/app/src/main/java/com/example/musicfinder/MainActivity.java
@@ -1,17 +1,32 @@
 package com.example.musicfinder;
 
+
 import android.content.Intent;
-import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
+import androidx.core.view.GravityCompat;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
+    private ImageView navigationIcon;
+    private MenuItem favItem, historyItem;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +38,26 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        setNavigationDrawerEvents();
+    }
+
+    private void setNavigationDrawerEvents() {
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
+        navigationIcon = findViewById(R.id.imageMenu);
+        Menu menu = navigationView.getMenu();
+
+        favItem = menu.findItem(R.id.nav_fav);
+        historyItem = menu.findItem(R.id.nav_history);
+
+        navigationIcon.setOnClickListener(this::openNavigationMenu);
+
+
+    }
+
+    public void openNavigationMenu(View view) {
+        drawerLayout.openDrawer(GravityCompat.END);
     }
 
     public void openArtistPage(View view) {
